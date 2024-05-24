d29b4de77eddfb71d3dbfadf1010b19b13e08985
superdarkhorse0903 <superdarkhorse0930@gmail.com>
2024-05-24 23:34:53 +1000

    test power move

--- src/game/commands/EquipCommand.ts
+++ src/game/commands/EquipCommand.ts
@@ -3,6 +3,10 @@ import { UfbRoom } from "#game/UfbRoom";
 import { isNullOrEmpty } from "#util";
 import { Client } from "colyseus";
 import { getClientCharacter } from "#game/helpers/room-helpers";
+import { Item } from "#game/schema/CharacterState";
+import { PowerMoveListMessage } from "#game/message-types";
+import { powermoves } from "#assets/resources";
+import { PowerMove } from "#shared-types";
 
 type OnEquipCommandPayload = {
     client: Client;
@@ -15,23 +19,57 @@ export class EquipCommand extends Command<UfbRoom, OnEquipCommandPayload> {
 
     execute({ client, message }: OnEquipCommandPayload) {
         const character = getClientCharacter(this.room, client);
-        character.stats.energy.add(2);
-        character.stats.coin += 2;
-        console.log("this is room id: haha. test mode.");
-
         if (!character) {
             this.room.notify(client, "You are not in room game!", "error");
         }
-        console.log("this is room id: haha. test mode.");
 
-        // const currentTile = this.state.map.tiles.get(character.currentTileId);
-        // const destinationTile = this.room.state.map.tiles.get(message.tileId);
+        // WHEN PLAYRE CLICK EQUIP POWER
+        const powerId = message.powerId;
+        
+        // REMOVE POWER in Array
+        const pIdx = character.powers.findIndex(p => p.id == powerId);
+        const power : Item = character.powers.find(p => p.id == powerId);
+        if(power == null || power.count == 0) {
+            console.log("count issue");
+            return;
+        }
+        power.count--;
+        console.log("power count: ", character.powers[pIdx].count);
 
-        // // this.room.broadcast("characterMoved", characterMovedMessage);
+        character.stats.energy.add(-1);
+        // SEND POWER MOVES
+        let clientMessage: PowerMoveListMessage = {
+            powermoves: []
+        }
+        powermoves.forEach(move => {
+            if(move.powerIds.indexOf(powerId) > -1) {
+                const powermove : PowerMove = {
+                    id : move.id,
+                    name : move.name,
+                    powerImageId : move.powerImageId,
+                    light : move.light,
+                    range : move.range,
+                    coin : move.coin,
+                    powerIds: [],
+                    costList: []
+                };
 
-        // if (character.stats.energy.current == 0) {
-        //     this.room.notify(client, "You are too tired to continue.");
-        //     this.room.incrementTurn();
-        // }
+                move.powerIds.forEach(pid => {
+                    powermove.powerIds.push(pid);
+                })
+                move.costList.forEach(cost => {
+                    const item = new Item();
+                    item.id = cost.id;
+                    item.count = cost.count;
+                    powermove.costList.push(
+                        item
+                    )
+                })
+                clientMessage.powermoves.push(powermove);
+            }
+        })
+
+        client.send("ReceivePowerMoveList", clientMessage);
+
     }
 }
--- src/game/message-handlers.ts
+++ src/game/message-handlers.ts
@@ -192,54 +192,13 @@ export const messageHandlers: MessageHandlers = {
 
 
     getPowerMoveList: (room, client, message) => {
-        // WHEN PLAYRE CLICK EQUIP POWER
-        const powerId = message.powerId;
-        const character = getClientCharacter(room, client);
+
+        room.dispatcher.dispatch(new EquipCommand(), {
+            client,
+            message
+        });
+
         
-        // REMOVE POWER in Array
-        const pIdx = character.powers.findIndex(p => p.id == powerId);
-        const power : Item = character.powers.find(p => p.id == powerId);
-        if(power == null || power.count == 0) {
-            console.log("count issue");
-            return;
-        }
-        power.count--;
-        console.log("power count: ", character.powers[pIdx].count);
-
-        character.stats.energy.add(-1);
-        // SEND POWER MOVES
-        let clientMessage: PowerMoveListMessage = {
-            powermoves: []
-        }
-        powermoves.forEach(move => {
-            if(move.powerIds.indexOf(powerId) > -1) {
-                const powermove : PowerMove = {
-                    id : move.id,
-                    name : move.name,
-                    powerImageId : move.powerImageId,
-                    light : move.light,
-                    range : move.range,
-                    coin : move.coin,
-                    powerIds: [],
-                    costList: []
-                };
-
-                move.powerIds.forEach(pid => {
-                    powermove.powerIds.push(pid);
-                })
-                move.costList.forEach(cost => {
-                    const item = new Item();
-                    item.id = cost.id;
-                    item.count = cost.count;
-                    powermove.costList.push(
-                        item
-                    )
-                })
-                clientMessage.powermoves.push(powermove);
-            }
-        })
-
-        client.send("ReceivePowerMoveList", clientMessage);
     },
 
     equipPower: (room, client, message) => {
