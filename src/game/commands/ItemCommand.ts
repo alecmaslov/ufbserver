c5bf596623a8f54528e690c26f1a6f54a3925b3f
Super Alchemist <sislight0224@gmail.com>
2025-06-30 13:41:57 -0700

    remove test part

--- src/game/commands/ItemCommand.ts
+++ src/game/commands/ItemCommand.ts
@@ -25,27 +25,27 @@ export class ItemCommand extends Command<UfbRoom, OnItemCommandPayload> {
         }
 
         // TEST:::
-        Object.keys(ITEMTYPE).forEach(key => {
-            const id = ITEMTYPE[key];
-            if(!(id == ITEMTYPE.RandomArrow || id == ITEMTYPE.RandomBomb)) {
-                const testItem : Item = character.items.find(item => item.id == id);
-                if(testItem == null) {
-                    const newItem = new Item();
-                    newItem.id = id;
-                    newItem.count = 30;
-                    newItem.name = ITEMDETAIL[id].name;
-                    newItem.description = "description";
-                    newItem.level = ITEMDETAIL[id].level;
-                    newItem.cost = ITEMDETAIL[id].cost;
-                    newItem.sell = ITEMDETAIL[id].sell;
+        // Object.keys(ITEMTYPE).forEach(key => {
+        //     const id = ITEMTYPE[key];
+        //     if(!(id == ITEMTYPE.RandomArrow || id == ITEMTYPE.RandomBomb)) {
+        //         const testItem : Item = character.items.find(item => item.id == id);
+        //         if(testItem == null) {
+        //             const newItem = new Item();
+        //             newItem.id = id;
+        //             newItem.count = 30;
+        //             newItem.name = ITEMDETAIL[id].name;
+        //             newItem.description = "description";
+        //             newItem.level = ITEMDETAIL[id].level;
+        //             newItem.cost = ITEMDETAIL[id].cost;
+        //             newItem.sell = ITEMDETAIL[id].sell;
         
-                    character.items.push(newItem);
-                } else {
-                    testItem.count++;
-                }
-            }
+        //             character.items.push(newItem);
+        //         } else {
+        //             testItem.count++;
+        //         }
+        //     }
 
-        });
+        // });
 
         // ADD STACKS
         // let k = 0;
@@ -69,23 +69,23 @@ export class ItemCommand extends Command<UfbRoom, OnItemCommandPayload> {
         // });
 
         // ADD POWER for MOVE ITEM
-        [POWERTYPE.Holy3, POWERTYPE.Void3, POWERTYPE.Armor3, POWERTYPE.Axe2, POWERTYPE.Spear3, POWERTYPE.Crossbow2, POWERTYPE.Cannon3, POWERTYPE.Ice3].forEach(key => {
-            const testPower : Item = character.powers.find(power => power.id == key);
-            if(testPower == null) {
-                const newPower = new Item();
-                newPower.id = key;
-                newPower.name = powers[key].name;
-                newPower.count = 4;
-                newPower.description = "";
-                newPower.level = powers[key].level;
-                newPower.cost = POWERCOSTS[powers[key].level].cost;
-                newPower.sell = POWERCOSTS[powers[key].level].sell;
+        // [POWERTYPE.Holy3, POWERTYPE.Void3, POWERTYPE.Armor3, POWERTYPE.Axe2, POWERTYPE.Spear3, POWERTYPE.Crossbow2, POWERTYPE.Cannon3, POWERTYPE.Ice3].forEach(key => {
+        //     const testPower : Item = character.powers.find(power => power.id == key);
+        //     if(testPower == null) {
+        //         const newPower = new Item();
+        //         newPower.id = key;
+        //         newPower.name = powers[key].name;
+        //         newPower.count = 4;
+        //         newPower.description = "";
+        //         newPower.level = powers[key].level;
+        //         newPower.cost = POWERCOSTS[powers[key].level].cost;
+        //         newPower.sell = POWERCOSTS[powers[key].level].sell;
 
-                character.powers.push(newPower);
-            } else {
-                testPower.count++;
-            }
-        })
+        //         character.powers.push(newPower);
+        //     } else {
+        //         testPower.count++;
+        //     }
+        // })
 
         // END TEST
 
